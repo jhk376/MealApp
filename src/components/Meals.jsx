f77a822b810826d49f519dd6165e25700324c93b
jhk376 <73979297+jhk376@users.noreply.github.com>
2022-10-31 19:36:13 +0800

    1031

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,15 @@
+import { useGlobalContext } from "../context"
+import { useEffect } from "react"
+
 const Meals =() =>{
-    return (<h1>Meals Component</h1>)   
+
+    const {meals} = useGlobalContext()
+    
+    return <sections>
+        {meals.map((singleMeal)=>{
+            console.log(singleMeal)
+            return <h4>single meal</h4>
+        })}
+    </sections>
 }
 export default Meals
--- src/context.jsx
+++ src/context.jsx
@@ -1,11 +1,37 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
+import axios from 'axios'
 
 const AppContext = React.createContext()
 
+const allMealsURL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
+const randomMealURL ='https://www.themealdb.com/api/json/v1/1/random.php'
+
 const AppProvider = ({children}) => {
-    return <AppContext.Provider value = "hello">
+    const [meals, setMeals] = useState([])
+
+    const fetchMeals = async (url) =>{
+        try{
+            const data = await axios(url)
+            setMeals(data.meals)
+            
+        } catch (error){    
+            console.log(error.response)
+
+        }
+    }
+
+    useEffect(() =>{    
+        //fetch().then
+        //.then async/await
+        //
+        fetchMeals(allMealsURL)
+    },[])
+    return <AppContext.Provider value = {{meals}}>
         {children}
     </AppContext.Provider>
 }
 
+export const useGlobalContext =() =>{
+    return useContext(AppContext)
+}
 export {AppContext, AppProvider}
